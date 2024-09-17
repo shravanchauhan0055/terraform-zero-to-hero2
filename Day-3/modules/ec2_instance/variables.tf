38d436ea7a66e3c05b4f5d235eb28caa3046d045
Shravan Chauhan <135550611+shravanchauhan0055@users.noreply.github.com>
2024-09-18 04:11:57 +0530

    Update variables.tf

--- Day-3/modules/ec2_instance/variables.tf
+++ Day-3/modules/ec2_instance/variables.tf
@@ -6,6 +6,6 @@ variable "instance_type_value" {
     description = "value for instance_type"
 }
 
-variable "subnet_id_value" {
-    description = "value for the subnet_id"
-}
+#variable "subnet_id_value" {
+#    description = "value for the subnet_id"
+#}
