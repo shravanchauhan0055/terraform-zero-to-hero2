0258c5553b7b0ed2139fcd38a49034b63357368d
Shravan Chauhan <135550611+shravanchauhan0055@users.noreply.github.com>
2024-09-18 04:13:26 +0530

    Update main.tf

--- Day-3/modules/ec2_instance/main.tf
+++ Day-3/modules/ec2_instance/main.tf
@@ -6,7 +6,6 @@ resource "aws_instance" "example" {
     key_name = "6sept"
     ami = var.ami_value
     instance_type = var.instance_type_value
-    #subnet_id = var.subnet_id_value
     tags = {
       Name = "modulevm1"
     }
